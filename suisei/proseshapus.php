baf6e1848ffcf855c873a8176f192bbbb88e6f41
RadhitViBritannia <110009337+RadhitViBritannia@users.noreply.github.com>
2022-11-01 11:52:33 +0700

    Delete suisei directory

--- suisei/proseshapus.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$id=$_GET['id'];
-
-$d = new PDO('mysql:host=localhost;dbname=pedulidiri','root',''); 
-$query = $d->query("DELETE FROM `tbisidata` WHERE `id`='$id' ");
-
-if($query){
-    header('location:catatan_perjalanan.php');}
